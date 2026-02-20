9c5cd647e91d3e4c8e40071055b98a7c2a1113a5
jackmcosta32 <joaomcosta32@gmail.com>
2026-02-20 15:54:59 -0300

    feat: cache refreshSessionTokens function for improved performance

--- apps/frontend/src/features/authentication/services/refresh-session-tokens.service.ts
+++ apps/frontend/src/features/authentication/services/refresh-session-tokens.service.ts
@@ -1,10 +1,11 @@
+import { cache } from 'react';
 import { httpApiClient } from '@/data/http/clients';
 
-export const refreshSessionTokens = async (options?: RequestInit) => {
+export const refreshSessionTokens = cache(async (options?: RequestInit) => {
   const response = await httpApiClient.request('/auth/refresh', {
     credentials: 'include',
     ...options,
   });
 
   return response.json();
-};
+});
